986f51917b2519e6d62bd12ffeb70243b49ed1e2
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-06-10 13:28:56 +0900

    first go commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+    // /helloエンドポイントにhelloHandler関数を登録
+    http.HandleFunc("/hello", helloHandler)
+
+    // サーバーをポート8080で起動
+    fmt.Println("Starting server at port 8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Println("Failed to start server: ", err)
+    }
+}
